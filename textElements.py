31e122c9c3216334e99ae9a83f41581e46dbe93d
osumi <osumiyuki1123@gmail.com>
2023-07-12 03:18:59 +0900

    textElementsで作成

---
+++ textElements.py
@@ -0,0 +1,18 @@
+import streamlit as st
+st.title('Sample App')
+st.markdown("""
+# 見出し1
+## 見出し2
+
+- 箇条書き1
+- 箇条書き2
+
+""")
+            
+st.code("""
+import numpy as np
+import pandas as pd
+a = 123
+pd.DataFrame()
+
+""")
